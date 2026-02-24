5ac4fde54862b76d1fe0dfeb3b6feae9d79cc65c
Georg Thiesen <georg.thiesen@rwth-aachen.de>
2026-02-24 08:37:05 +0100

    init commit

---
+++ src/http_request.rs
@@ -0,0 +1,18 @@
+use reqwest::{Error};
+
+
+pub async fn send_get_request(url: &str) -> Result<String, Error> {
+
+    let client = reqwest::Client::new();
+
+    let response = client.post(url)
+        .header("User-Agent", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:130.0) Gecko/20100101 Firefox/130.0")
+        .header("Connection", "keep-alive")
+        .header("Accept-Language", "en-US,en;q=0.5")
+        .header("Content-Type", "application/json")
+        //.json(&body)
+        .send()
+        .await?;
+    
+    response.text().await
+}
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+pub mod http_request;
+
+#[tokio::main]
+async fn main() {
+    println!("Hello, world!");
+}
